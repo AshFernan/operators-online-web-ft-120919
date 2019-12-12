47f55bf8634931162fafe7d721f1e745bb24c25c
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-12 22:44:16 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,21 @@
-def unsafe?(speed)
-
+def unsafe(speed = 60)
+  if speed < 79
+     return "speed is greater than 60" 
+  elsif 
+    speed > 35 
+    return "speed is less than 40" 
+  else 
+    "false"
+  end
 end
 
-
-
-def not_safe?(speed)
+def not_safe (speed = 60) 
+  
+speed < 79 ? "speed is over than 60" : "This is false"
 	
+speed < 35 ? "speed is less than 40" : "This is false"
 end
+
 	
 
 
